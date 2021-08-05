3ad68b39522906b0c98324cce073c7354cdbae60
RaulORios <raul.orios89@outlook.com>
2021-08-05 12:21:07 -0300

    Program.cs adicionado

---
+++ src/EXERCICIO_VETOR_FOREACH/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EXERCICIO_VETOR_FOREACH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                nomes[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Lista de nomes: ");
+            foreach(string elemento in nomes)
+            {
+                Console.WriteLine(elemento);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
